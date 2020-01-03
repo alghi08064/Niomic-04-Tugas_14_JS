1ec62b61ea7fea6050557c3dd07be8924a40ab68
Alghifari Muhammad Dzar <alghifari.m.d@gmail.com>
2020-01-03 12:59:09 +0300

    Tugas 14 JS
    Tugas ini untuk Niomic

---
+++ Tugas14.js
@@ -0,0 +1,7 @@
+var k = 26;
+u = 37/k;
+
+console.log('Hasil asli:',u)
+console.log('Hasil dengan pembulatan ke atas:',Math.ceil(u))
+console.log('Hasil dengan pembulatan ke bawah:',Math.floor(u))
+console.log('Hasil dengan pembulatan acuan 0.5:',Math.round(u))
